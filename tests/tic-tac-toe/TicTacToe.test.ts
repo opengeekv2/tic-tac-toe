4ec4a7f42d1db42796e5021058ea316ff4feb26c
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-01-27 16:58:38 +0100

    Refactor extract TicTacToe initialization to afterEach

--- tests/tic-tac-toe/TicTacToe.test.ts
+++ tests/tic-tac-toe/TicTacToe.test.ts
@@ -1,21 +1,25 @@
 import TicTacToe from "../../src/TicTacToe";
 
 describe("Tic Tac Toe", () => {
+
+    let ticTacToe: TicTacToe = new TicTacToe();
+
+    afterEach(() => {
+        ticTacToe = new TicTacToe();
+    });
+
     it("should allow player X to start", () => {
-        const ticTacToe: TicTacToe = new TicTacToe();
         const output = ticTacToe.play({x: 0, y: 0}, 'X');
         expect(output).toBe(true);
     });
 
     it("should not allow player O to start", () => {
-        const ticTacToe: TicTacToe = new TicTacToe();
         expect(() => {
             ticTacToe.play({x: 0, y: 0}, 'O');
         }).toThrowError();
-    })
+    });
 
     it("should not allow a unknown player", () => {
-        const ticTacToe: TicTacToe = new TicTacToe();
         const unknownPlayer = 'T';
         expect(() => {
             ticTacToe.play({x: 0, y: 0}, unknownPlayer);
@@ -23,19 +27,16 @@ describe("Tic Tac Toe", () => {
     })
 
     it("should switch player X to O", () => {
-        const ticTacToe: TicTacToe = new TicTacToe();
         ticTacToe.play({x: 0, y: 0}, 'X');
         const output = ticTacToe.play({x: 0, y: 0}, 'O');
         expect(output).toBe(true);
-    })
+    });
 
     it("should not let X player play twice", () => {
-        const ticTacToe: TicTacToe = new TicTacToe();
         ticTacToe.play({x: 0, y: 0}, 'X');
          expect(() => {
             ticTacToe.play({x: 0, y: 0}, 'X');
         }).toThrowError();
-    })
+    });
 
-
-})
+});
